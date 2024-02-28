1f05fbe1c1521c801b2a75de915fce584720695a
Chris070807 <55923806+Chris070807@users.noreply.github.com>
2024-02-27 22:28:05 -0600

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,77 @@
-
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (currentDirection != Direction.Down) {
+        currentDirection = Direction.Up
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (currentDirection != Direction.Right) {
+        currentDirection = Direction.Left
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (currentDirection != Direction.Left) {
+        currentDirection = Direction.Right
+    }
+})
+function moveSnake () {
+    if (currentDirection == Direction.Up) {
+        newHead = sprites.create(snakeHead.image, SpriteKind.Player)
+        newHead.setPosition(snakeHead.x, snakeHead.y - 8)
+    } else if (currentDirection == Direction.Down) {
+        newHead = sprites.create(snakeHead.image, SpriteKind.Player)
+        newHead.setPosition(snakeHead.x, snakeHead.y + 8)
+    } else if (currentDirection == Direction.Left) {
+        newHead = sprites.create(snakeHead.image, SpriteKind.Player)
+        newHead.setPosition(snakeHead.x - 8, snakeHead.y)
+    } else if (currentDirection == Direction.Right) {
+        newHead = sprites.create(snakeHead.image, SpriteKind.Player)
+        newHead.setPosition(snakeHead.x + 8, snakeHead.y)
+    }
+    if (newHead) {
+        snakeBody.unshift(newHead)
+        snakeHead = newHead
+        if (snakeBody.length > 1) {
+            tail = snakeBody.pop()
+            tail.destroy()
+        }
+    }
+}
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (currentDirection != Direction.Up) {
+        currentDirection = Direction.Down
+    }
+})
+let timeSinceLastMove = 0
+let tail: Sprite = null
+let newHead: Sprite = null
+let snakeHead: Sprite = null
+let snakeBody: Sprite[] = []
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
+snakeHead = sprites.create(img`
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    . . . . 2 2 2 . . . 
+    . . . 2 2 2 2 2 . . 
+    . . . 2 2 2 2 2 . . 
+    . . 2 2 2 2 2 2 . . 
+    . . 2 2 2 2 2 . . . 
+    . . 2 2 2 . . . . . 
+    . . . . . . . . . . 
+    . . . . . . . . . . 
+    `, SpriteKind.Player)
+snakeBody.push(snakeHead)
+let currentDirection = Direction.Right
+let gameSpeed = 500
+scene.setBackgroundColor(7)
+game.onUpdate(function () {
+    timeSinceLastMove += game.eventContext().deltaTimeMillis
+    if (timeSinceLastMove >= gameSpeed) {
+        moveSnake()
+        timeSinceLastMove = 0
+    }
+})
